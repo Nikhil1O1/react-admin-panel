72739a068226a4fd95150d8c26d70cd38a71ba9b
Nikhil1O1 <technikue2.0@gmail.com>
2023-09-15 17:31:03 +0530

    working navbar and optimized sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,16 +8,23 @@ import {links} from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const {activeMenu,setActiveMenu} = useStateContext();
+  const {activeMenu,setActiveMenu, screenSize} = useStateContext();
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-grey-200 dark:hover:text-black hover:bg-light-gray m-2'
+  const handleCloseSidebar=()=>{
+    if (activeMenu&&screenSize<=900){
+      setActiveMenu(false)
+    }
+  }
+  // handeCloseSidebar allows sidebar to close if screen is small like mobile and user clicks
+  // on a side bar link. If screen is big the sidebar stays on screen
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto
     pb-10'>
     {
       activeMenu&&(<>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={()=>setActiveMenu(false)}
+          <Link to="/" onClick={handleCloseSidebar}
           className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold 
           tracking-tight dark:text-white text-slate-900'>
             <SiShopware/> <span>Shoppy</span>
@@ -38,7 +45,7 @@ const Sidebar = () => {
                     {item.links.map((link)=>(
                       <NavLink to={`/${Link.name}`}
                       key={Link.name}
-                      onCanPlay={()=>{}}
+                      onClick={handleCloseSidebar}
                       className={({isActive})=>isActive?activeLink:normalLink}>
                         {link.icon}
                         <span className='capitalize'>
